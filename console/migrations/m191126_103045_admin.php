e527b2b13ab37fac3c7a83e9b05ebca199bd50d7
stasadd <stas.add@gmail.com>
2019-12-06 23:51:10 +0200

    cart commit

---
+++ console/migrations/m191126_103045_admin.php
@@ -0,0 +1,56 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Schema;
+
+/**
+ * Class m191126_103045_admin
+ */
+class m191126_103045_admin extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql'){
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=INNODB';
+        }
+        $this->createTable('{{%admin}}',[
+            'id' => Schema::TYPE_PK,
+            'username' => $this->string()->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ],$tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%admin}}');
+        echo "m191126_103045_admin cannot be reverted.\n";
+
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191126_103045_admin cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
