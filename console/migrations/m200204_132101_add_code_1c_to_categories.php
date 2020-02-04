820394ce2595f2bacfcc8bec1e6aabd9f05b0fd8
stasadd <stas.add@gmail.com>
2020-02-04 23:09:06 +0200

    categories added

--- console/migrations/m200204_132101_add_code_1c_to_categories.php
+++ console/migrations/m200204_132101_add_code_1c_to_categories.php
@@ -9,11 +9,16 @@ class m200204_132101_add_code_1c_to_categories extends Migration
 {
     public function up()
     {
-        $this->addColumn('{{%categories}}', 'code_1c', $this->string()->defaultValue(null));
+
+        $this->addColumn('{{%categories}}', 'code_1c', $this->integer()->defaultValue(null));
+        $this->createIndex('index_categories_code_1c','{{%categories}}','code_1c');
     }
 
     public function down()
     {
         $this->dropColumn('{{%categories}}', 'code_1c');
+        $this->dropIndex('index_categories_code_1c','{{%categories}}' );
+
+        return true;
     }
 }
